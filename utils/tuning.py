0ba34f73b89f40b792d91243e5f6335ce2fd60c0
yvjennig <yvonne.jenniges@awi.de>
2025-12-15 13:51:23 +0100

    If a result file already exists, return that one

--- utils/tuning.py
+++ utils/tuning.py
@@ -74,6 +74,10 @@ def set_seed(seed):
 
 
 def combine_csvs(dir_path, out_name="combined.csv", remove_files=False):
+    # Load file if exists
+    if Path.exists(Path(dir_path + out_name)):
+        return pd.read_csv(dir_path + out_name)
+
     # Load all result files as df
     res_files = glob.glob(f"{dir_path}/*.csv")
 
