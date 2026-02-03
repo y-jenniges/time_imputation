60451eca31ed9cd38f31cbec29eff0bd07a8895a
yvjennig <yvonne.jenniges@awi.de>
2026-02-03 16:12:19 +0100

    Change dimensionality of x so we have input (batch_size, set_size, features); remove custom dropout layer, use encoderlayer dropout instead

--- models/mae.py
+++ models/mae.py
@@ -9,14 +9,14 @@ class OceanMAE(nn.Module):
         self.coord_dim = coord_dim
         self.value_dim = value_dim
         self.input_dim = self.coord_dim + self.value_dim * 2  # coords, features, feature masks
-        self.dropout = nn.Dropout(dropout)
 
         # Input encoder
         self.encoder_input = nn.Linear(self.input_dim, d_model)
 
         # Transformer encoder
         encoder_layer = nn.TransformerEncoderLayer(
-            d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward, activation="gelu", batch_first=True
+            d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward, activation="gelu", batch_first=True,
+            dropout=dropout
         )
         self.encoder = nn.TransformerEncoder(encoder_layer, nlayers)
 
@@ -32,20 +32,17 @@ class OceanMAE(nn.Module):
             nn.Softplus()
         )
 
-    def forward(self, coords, values, feature_mask, mc_dropout=False):
+    def forward(self, coords, values, feature_mask):
         # Fill nans with 0 (in features)
         values_filled = torch.where(torch.isnan(values), torch.zeros_like(values), values)
-        x = torch.cat([coords, values_filled, feature_mask.float(), ], dim=-1)
+        x = torch.cat([coords, values_filled, feature_mask.float()], dim=-1)
 
-        # Encoding input and dropout
+        # Encoding input
         x = self.encoder_input(x)
-        if mc_dropout:
-            x = self.dropout(x)
-
-        encoded = self.encoder(x.unsqueeze(1)).squeeze(1)
+        encoded = self.encoder(x)
 
         # Predict mean and variance
         pmean = self.mean_decoder(encoded)
-        pvar = torch.exp(self.var_decoder(encoded))
+        pvar = self.var_decoder(encoded)
 
         return pmean, pvar
