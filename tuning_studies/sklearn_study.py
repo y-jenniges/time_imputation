96f4d339d8d76b000ef9292567c4fc1fbc73e4e0
yvjennig <yvonne.jenniges@awi.de>
2026-03-19 12:58:27 +0100

    Fixing hyp ranges

--- tuning_studies/sklearn_study.py
+++ tuning_studies/sklearn_study.py
@@ -126,7 +126,7 @@ def suggest_hyperparameters(trial, model_name):
         return {
             "max_iter": trial.suggest_categorical("max_iter", [100, 300, 1000]),
             "initial_strategy": trial.suggest_int("initial_strategy", 0, len(base_model.initial_strategy_vals)-1),  # ["mean", "median", "most_frequent", "constant"]),
-            "imputation_order": trial.int("imputation_order", 0, len(base_model.imputation_order_vals)-1),  # ["ascending", "descending", "roman", "arabic", "random"]),
+            "imputation_order": trial.suggest_int("imputation_order", 0, len(base_model.imputation_order_vals)-1),  # ["ascending", "descending", "roman", "arabic", "random"]),
             "random_state": 42,
         }
     elif model_name == "missforest_hyperimpute":
