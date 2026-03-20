20dbb259b94d7a3068ff1d502f6dbcb2f90dd0e7
yvjennig <yvonne.jenniges@awi.de>
2026-03-20 16:38:33 +0100

    adding graph provider

---
+++ nn_utils/graph.py
@@ -0,0 +1,45 @@
+import torch
+from sklearn.neighbors import NearestNeighbors
+
+
+class GraphProvider:
+    def __init__(self, n_neighbours=20, update_every: int = 5, test_idx: torch.Tensor = None, val_idx: torch.Tensor = None):
+        self.n_neighbours = n_neighbours
+        self.update_every = update_every
+        self.neighbour_indices = None
+
+        self.test_idx = test_idx
+        self.val_idx = val_idx
+
+    def update(self, encoder, coords, values, mask):
+        """ Recompute graph from latent space. """
+        print("Graph size:", coords.shape)
+        values = values.clone()
+        mask = mask.clone()
+
+        if self.test_idx is not None:
+            values[self.test_idx] = torch.nan
+
+        if self.val_idx is not None:
+            values[self.val_idx] = torch.nan
+
+        if self.neighbour_indices is not None:
+            print("Neighbour index shape:", self.neighbour_indices.shape)
+
+        with torch.no_grad():
+            # Encode
+            encoded = encoder(coords=coords[:, :4], values=torch.nan_to_num(values, nan=0.0), mask=mask.float(), t_fourier=coords[:, -1:].float())
+            encoded_np = encoded.detach().cpu().numpy()
+
+            # Construct KNN graph
+            knn = NearestNeighbors(n_neighbors=self.n_neighbours + 1)
+            knn.fit(encoded_np)
+
+            # Get neighbour indices
+            indices = torch.from_numpy(knn.kneighbors(encoded_np, return_distance=False)).long()
+            self.neighbour_indices = indices[:, 1:]  # Remove self
+
+            print("Neighbour index shape after update: ", self.neighbour_indices.shape)
+
+    def get(self, idx):
+        return self.neighbour_indices[idx]
